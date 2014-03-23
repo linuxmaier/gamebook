ca61c22144b1357767ce689c43cc9b76422835cd
linuxmaier <andrew.eric.maier@gmail.com>
2014-03-23 17:52:01 -0500

    basic models for author and gamebook

--- models.py
+++ models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class GameBook(models.Model):
+    title = models.CharField(max_length=50)
+    pub_date = modelsDateTimeField('date published')
+    pages = models.
+
+
+class Author(models.Model):
+    full_name = models.CharField(max_length=50)
+    email_addr = models.CharField(max_length=254)
+    gamebooks = models.ForeignKey(GameBook, related_name='author')
+    bio = models.TextField()
+
