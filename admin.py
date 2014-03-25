2b78b9778a3df8e578b4aa52e87872b50ca72a25
linuxmaier <andrew.eric.maier@gmail.com>
2014-03-25 18:21:17 -0500

    added admin site hooks

--- admin.py
+++ admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from gamebook.models import GameBook, Author, Page, Choice
 
-# Register your models here.
+
+admin.site.register(Author)
+admin.site.register(GameBook)
+admin.site.register(Page)
+admin.site.register(Choice)
+
