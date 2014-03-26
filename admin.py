f5629ae5e6517ba4e6ef8ba8dc9a0d4b02e01ddc
linuxmaier <andrew.eric.maier@gmail.com>
2014-03-25 19:11:03 -0500

    show pages of gamebook in its admin interface

--- admin.py
+++ admin.py
@@ -2,8 +2,18 @@ from django.contrib import admin
 from gamebook.models import GameBook, Author, Page, Choice
 
 
+class PageInline(admin.TabularInline):
+    model = Page
+
+
+class GameBookAdmin(admin.ModelAdmin):
+    inlines = [
+        PageInline,
+    ]
+
+
 admin.site.register(Author)
-admin.site.register(GameBook)
+admin.site.register(GameBook, GameBookAdmin)
 admin.site.register(Page)
 admin.site.register(Choice)
 
