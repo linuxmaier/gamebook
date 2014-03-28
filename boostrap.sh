c4d5f755195bb2b284ddbaf3321ed606501a7caf
Andrew Maier <andrew.eric.maier@gmail.com>
2014-03-28 13:43:19 -0500

    automatic vagrant provisioning

---
+++ boostrap.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+#update package list and upgrade packages
+apt-get update && apt-get upgrade -y
+
+#install git
+apt-get install -y git
+
+#install tmux
+apt-get install -y tmux
+
+#install python related packages
+apt-get install -y python-mysqldb
+apt-get install -y python-pip
+
+#install packages from pip
+pip install django
+pip install south
+
+
